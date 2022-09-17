0a173b526e7973547f9b239e9cb7551d23edfc1d
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-09-17 21:26:01 +0530

    changes-2022-09-17-21-26-01

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
-import express from "express"
+import express, { response } from "express"
 import cors from "cors"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use("*",(req,res)=>res.status(404).json({error:"url not found !!"}))
+
+export default app
