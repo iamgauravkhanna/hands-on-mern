0ab49ddec4491a63d64887dd7cc9f9edde7af869
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-09-19 07:51:53 +0530

    changes-2022-09-19-07-51-53

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-import express, { response } from "express"
+import express from "express"
 import cors from "cors"
 
-const app = express()
+const app = express();
+const port = 3001;
 
 app.use(cors())
 app.use(express.json())
 
-app.use("*",(req,res)=>res.status(404).json({error:"url not found !!"}))
-
-export default app
+app.listen(port,()=> {
+console.log(`Server is running on port: ${port}`)
+});
