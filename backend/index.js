a92a73b35fe441ade1ba00bb6c2f628afa513abe
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-09-19 09:49:10 +0530

    changes-2022-09-19-09-49-10

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import app from "./server.js"
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const MongoClient = mongodb.MongoClient
+const port = process.env.PORT
+
+MongoClient.connect(
+    process.env.DB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+)
+.catch(err =>{
+    console.log('ERROR OCCURED......!!!')
+    console.log(err.stack)
+    process.exit(1)
+})
+.then(async client =>{
+    app.listen(port,()=>{
+        console.log(`Backend is Up. Listening to port: ${port}`)
+    })
+})
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from "express"
 import cors from "cors"
+import restaurants from "./api/restaurants.router.js"
 
 const app = express();
 const port = 3001;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors()) ;
+app.use(express.json()) ;
 
-app.listen(port,()=> {
-console.log(`Server is running on port: ${port}`)
-});
+app.use("/api/v1/restaurants",restaurants);
+
+export default app ;
